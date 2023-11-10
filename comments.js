aebec6d2b6fb4081755b2f438ee8da150970fb90
JshJLV <90582121+JshJLV@users.noreply.github.com>
2023-11-10 00:24:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,63 @@
-// create a web server 
-// 1. create a web server
-// 2. create a route
-// 3. create a handler
-// 4. send a response
-// 5. start the server
+// Create web server using express
+// Create a GET route for /comments that returns the comments array
+// Create a POST route for /comments that adds a new comment to the array
+// Create a DELETE route for /comments that will delete the comment with the specified ID
+// Create a PUT route for /comments that will update the comment with the specified ID
+// Create a GET route for /comments/:id that returns the comment with the specified ID
+// Create a GET route for /post-comments/:postId that returns the comments for the specified post
+// Create a GET route for /users/:userId/comments that returns the comments for the specified user
 
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const router = express.Router();
 
-// 2. create a route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const { comments } = require("../data/comments");
+
+router.get("/", (req, res) => {
+  res.json(comments);
 });
 
-// 3. create a handler
-app.get('/comments', (req, res) => {
-    res.send('This is a comments page');
+router.get("/:id", (req, res) => {
+  const commentId = parseInt(req.params.id);
+  const foundComment = comments.find((comment) => comment._id === commentId);
+  res.json(foundComment);
 });
 
-// 4. send a response
-app.get('/comments/:id', (req, res) => {
-    res.send(`This is a comments page for ${req.params.id}`);
+router.get("/post-comments/:postId", (req, res) => {
+  const postId = parseInt(req.params.postId);
+  const foundComments = comments.filter(
+    (comment) => comment.postId === postId
+  );
+  res.json(foundComments);
 });
 
-// 5. start the server
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+router.get("/users/:userId/comments", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const foundComments = comments.filter((comment) => comment.userId === userId);
+  res.json(foundComments);
 });
+
+router.post("/", (req, res) => {
+  const newComment = {
+    _id: comments.length + 1,
+    body: req.body.body,
+    postId: req.body.postId,
+    userId: req.body.userId,
+  };
+  comments.push(newComment);
+  res.json(comments);
+});
+
+router.put("/:id", (req, res) => {
+  const commentId = parseInt(req.params.id);
+  const foundComment = comments.find((comment) => comment._id === commentId);
+  foundComment.body = req.body.body;
+  foundComment.postId = req.body.postId;
+  foundComment.userId = req.body.userId;
+  res.json(comments);
+});
+
+router.delete("/:id", (req, res) => {
+  const commentId = parseInt(req.params.id);
+  const foundComment = comments.find((comment) => comment._id === commentId);
+  const index = comments
+
